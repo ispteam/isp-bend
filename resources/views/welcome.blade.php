b55a6e8b8de65b6945001721e6b260110a728a59
fatema983 <f.m.rahal88@gmail.com>
2021-06-03 04:31:23 +0300

    init

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYPE html>//
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -121,7 +121,7 @@
                             </a>
                         </div>
                     </div>
-
+                    
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
