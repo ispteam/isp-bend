7130db8da39fc6237855da5b732517d86daf9b35
fatema983 <f.m.rahal88@gmail.com>
2021-06-03 17:54:57 +0300

    init

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html >
+<head>
+   <h1>Welcome 111</h2>
+</head>
+<body>
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <h3>Selecao</h3>
+      <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
+      <div class="social-links">
+        <a href="#">Home</a>
+        <a href="#">Requests></a>
+        <a href="#">For Suppliers</a>
+        <a href="#"> Contact</a>
+   
+      </div>
+      <div class="copyright">
+        &copy; Copyright <strong><span>Selecao</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/selecao-bootstrap-template/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+</body>
+</html>
+
