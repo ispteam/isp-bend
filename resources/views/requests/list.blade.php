2d856c06acb063a3649a3259b9aec23c1548776a
fatema983 <f.m.rahal88@gmail.com>
2021-06-04 03:41:26 +0300

    init

---
+++ resources/views/requests/list.blade.php
@@ -0,0 +1,46 @@
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>List of requests</title>
+    </head>
+        
+<body>
+<h1>List of requests</h1>
+<p>Choose brands</p>
+<form action="/action_page.php">
+  <select name="cars" id="cars">
+    <option value="volvo">Volvo</option>
+    <option value="saab">Saab</option>
+    <option value="opel">Opel</option>
+    <option value="audi">Audi</option>
+  </select>
+  <br><br>
+
+</form>
+
+<h1>Choose countries</h1>
+
+<form action="/action_page.php">
+  <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+  <label for="vehicle1"> Africa</label><br>
+  <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
+  <label for="vehicle2"> Europe</label><br>
+  <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+  <label for="vehicle3">North America</label><br>
+  <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+  <label for="vehicle3">South America</label><br>
+  <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+  <label for="vehicle3">Asia</label><br>
+  <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+  <label for="vehicle3">Australia & Oceania</label><br>
+  <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+  <label for="vehicle3">Antarctic</label><br><br>
+  <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
+
+
